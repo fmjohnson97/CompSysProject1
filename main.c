ca698f203a5d83e094f16f2ae5d690d1f4b058e1
Faith Johnson <fmjohnson97@aol.com>
2018-03-02 18:30:09 -0500

    Problem 1

---
+++ main.c
@@ -0,0 +1,50 @@
+//
+//  main.c
+//  Project1
+//
+//  Created by Faith Johnson on 2/28/18.
+//  Copyright © 2018 Faith Johnson. All rights reserved.
+//
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    pid_t pid=fork();
+    pid_t pid2=fork();
+    int status;
+    int retCode;
+
+    if(pid==0 && pid2==0)
+    {
+        printf("D, my pid=%d, Parent's pid=%d\n",getpid(),getppid());
+        return 12;
+    }
+    else if (pid==0)
+    {
+        waitpid(pid2,&status,0);
+        int retCode=WEXITSTATUS(status);
+        printf("\tD's exit code = %d\n",retCode);
+        printf("B, my pid=%d, Parent's pid=%d\n",getpid(),getppid());
+        return 8;
+    }
+    else if (pid2==0)
+    {
+        printf("C, my pid=%d, Parent's pid=%d\n",getpid(),getppid());
+        return 9;
+    }
+    else
+    {
+        waitpid(pid,&status,0);
+        retCode=WEXITSTATUS(status);
+        printf("\tB's exit code = %d\n",retCode);
+        waitpid(pid2,&status,1);
+        retCode=WEXITSTATUS(status);
+        printf("C's exit code = %d\n",retCode);
+        printf("A, my pid=%d, my exit code=%d\n",getpid(),5);
+        return 5;
+    }
+}
