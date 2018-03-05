83fba784f005b64ae92855ac5987594e652b2f73
Faith Johnson <fmjohnson97@aol.com>
2018-03-05 14:34:36 -0500

    Problem 1

--- main.c
+++ main.c
@@ -13,6 +13,7 @@
 
 int main(void)
 {
+    printf("Parent PID = %d\n",getpid());
     pid_t pid=fork();
     pid_t pid2=fork();
     int status;
@@ -20,31 +21,42 @@ int main(void)
 
     if(pid==0 && pid2==0)
     {
-        printf("D, my pid=%d, Parent's pid=%d\n",getpid(),getppid());
+        printf("Start D with pid=%d\n",getpid());
+        //printf("D, my pid=%d, Parent's pid=%d\n",getpid(),getppid());
+        printf("Terminate D with pid=%d\n",getpid());
         return 12;
     }
     else if (pid==0)
     {
+        printf("Start B with pid=%d\n",getpid());
+        printf("Wait for Child D with pid=%d\n", pid2);
         waitpid(pid2,&status,0);
         int retCode=WEXITSTATUS(status);
         printf("\tD's exit code = %d\n",retCode);
-        printf("B, my pid=%d, Parent's pid=%d\n",getpid(),getppid());
+        //printf("B, my pid=%d, Parent's pid=%d\n",getpid(),getppid());
+        printf("Terminate B with pid=%d\n",getpid());
         return 8;
     }
     else if (pid2==0)
     {
-        printf("C, my pid=%d, Parent's pid=%d\n",getpid(),getppid());
+        printf("Start C with pid=%d\n",getpid());
+        //printf("C, my pid=%d, Parent's pid=%d\n",getpid(),getppid());
+        printf("Terminate C with pid=%d\n",getpid());
         return 9;
     }
     else
     {
+        printf("Start A with pid=%d\n",getpid());
+        printf("Wait for child C with pid=%d\n",pid2);
+        waitpid(pid2,&status,0);
+        retCode=WEXITSTATUS(status);
+        printf("C's exit code = %d\n",retCode);
+        printf("Wait for child B with pid=%d\n", pid);
         waitpid(pid,&status,0);
         retCode=WEXITSTATUS(status);
         printf("\tB's exit code = %d\n",retCode);
-        waitpid(pid2,&status,1);
-        retCode=WEXITSTATUS(status);
-        printf("C's exit code = %d\n",retCode);
-        printf("A, my pid=%d, my exit code=%d\n",getpid(),5);
+        //printf("A, my pid=%d, my exit code=%d\n",getpid(),5);
+        printf("Terminate A with pid=%d\n",getpid());
         return 5;
     }
 }
