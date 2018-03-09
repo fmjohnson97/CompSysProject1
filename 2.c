25db848561a2a124abf42983aff42cae6522a6c9
Mitch Gin Lew <37005067+ChuLew@users.noreply.github.com>
2018-03-09 04:54:02 -0500

    Update 2.c

--- 2.c
+++ 2.c
@@ -59,9 +59,6 @@ tree_node* createTree(tree_node *thestruct, char* ident) // when node has 0 chil
        
     }
     sprintf(thestruct->line,"%s",buffer);     
-    //printf("%s",thestruct->line);
-    //printf("%s",buffer);
-    //fflush(stdout);
     char *rootNode = strtok(buffer," ");
     
     char *strNumChild = strtok(NULL," ");
@@ -85,7 +82,7 @@ tree_node* createTree(tree_node *thestruct, char* ident) // when node has 0 chil
         if (pid==0)
         {
             createTree(thestruct, stream);
-            //wait(NULL);
+
             pid = getpid();
             thestruct->pid = pid; 
                   
@@ -121,7 +118,7 @@ tree_node* createTree(tree_node *thestruct, char* ident) // when node has 0 chil
                 {  
                     createTree(thestruct, firstBorn->data);  
                 }         
-               //wait(NULL);
+
                pid = getpid();
                thestruct->pid = pid; 
                return thestruct;
@@ -130,17 +127,19 @@ tree_node* createTree(tree_node *thestruct, char* ident) // when node has 0 chil
 void print_tree(tree_node *thestruct,int* fd,int numKids)
 {    
     printf("%s",thestruct->line);
+    fflush(stdout);
     int i = 0;
     char buff[20];
-    //for(i = numKids; i > 0; i--)
-        for(i=1;i<numKids+1;i++)
-        {  
-            bzero(buff,20);
-            close(fd[2*i+1]);
-            read(fd[2*i], buff, 20);
-            close(fd[2*i]);
-            printf("%s",buff);
-        }
+     
+    for(i=1;i<numKids+3;i++)
+    {  
+        bzero(buff,20);
+        close(fd[2*i+1]);
+        read(fd[2*i], buff, 20);
+        close(fd[2*i]);
+        printf("%s",buff);
+        fflush(stdout);
+    }
         
       
         
@@ -150,15 +149,13 @@ void print_tree(tree_node *thestruct,int* fd,int numKids)
 int main(int argc, char *argv[])
 {       
     //FILE *fo = freopen("output2.txt","w",stdout);
+    
     char* fileName = argv[1];
     char buffer[100];
     char* init="";
     char* strNumChild;
     int numKids=0;
     pid_t pid = getpid();
-    
-    //printf("The main PID: %ld\n", (long) pid);
-    
     FILE* fp;
     FILE* countChildren;
     FILE* sc;
@@ -168,6 +165,7 @@ int main(int argc, char *argv[])
     int procNum = 0; 
     char* letter;
     char convert[1]; 
+    
     while(fgets(buffer,100,countChildren))
     {
         strtok(buffer," ");
@@ -175,7 +173,8 @@ int main(int argc, char *argv[])
         numKids += atoi(strNumChild); 
         
     }
-      char* pni = (char*)malloc((numKids*2)*sizeof(char)); // pni = process number identfier 
+    
+    char* pni = (char*)malloc((numKids*2)*sizeof(char)); // pni = process number identfier 
       
     while(fgets(buffer,100,sc))
     {
@@ -188,11 +187,10 @@ int main(int argc, char *argv[])
         procNum++;
         
     }  
-      // printf("%s\n",pni);
-    
-   // printf("numKids: %d\n",numKids);
+      
     int fd[2*numKids];
     int i = 0;
+    
     for(i=0;i<numKids;i++)
     {
         pipe(&fd[2*i]);
@@ -204,11 +202,10 @@ int main(int argc, char *argv[])
     initNode->numChildren = 0;
     initNode = createTree(initNode, init);
     
-    //printf("ProcessNumber:%d,Root:%s\n",initNode->procNumber,initNode->rootNode);
       char* rootNode = initNode->rootNode;
       pid_t dummyPID = initNode->pid;
       procNum = initNode->procNumber;
-     // printf("procNum:%d\n",procNum);
+
       if(dummyPID == pid)
       {  
         
@@ -217,10 +214,8 @@ int main(int argc, char *argv[])
       else{//child process stuff 
          close(fd[2*procNum]);
          char* line = strdup(initNode->line); 
-         //printf("%s",line);
          write(fd[2*procNum+1],line,20);
       } 
-    
-
+     //fclose(fo);
 }
 
